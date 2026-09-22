18658887ade311ce15b83ee388240de65d471812
Aydea0260 <128005270+Aydea0260@users.noreply.github.com>
2026-09-21 21:31:38 -0400

    Config: URL de Apps Script conectada

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
  * Mientras esté el valor por defecto, el formulario muestra el resultado igual
  * y guarda el envío en segundo plano sin romper nada.
  */
-const APPS_SCRIPT_URL = "PEGAR_URL_AQUI";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw20jJG5pjqJc1tXr2fA9n8AWUOCFgxvQgGW9HR1VE2TMmMClkyUoCi8s_CGuz3PWs0/exec";
